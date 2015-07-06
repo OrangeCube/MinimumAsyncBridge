ce071af6d3fed3a94a60d029c84e97640431640b
niwanaga <iwanaga@orange-cube.jp>
2015-07-06 19:00:33 +0900

    Add an Unity sample

---
+++ SampleUnityProject/Assets/Scripts/SampleBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using SampleModels;
+using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using I = IteratorTasks;
+
+public class SampleBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    Text _textOutput;
+
+    void Start()
+    {
+        var s = new Subject<string>();
+
+        var scheduler = SchedulerUnity.MainThread;
+
+        s.ObserveOn(scheduler).Subscribe(
+            result =>
+            {
+                Debug.Log(result);
+                _textOutput.text += result.Substring(0, Math.Min(result.Length, 100)) + "\n";
+            },
+            ex =>
+            {
+                Debug.Log(ex);
+                _textOutput.text += ex.Message + "\n";
+            }
+            );
+
+        Class1.MainAsync(s);
+    }
+
+    void Update()
+    {
+        var s = I.Task.DefaultScheduler;
+        s.Update();
+    }
+}
