349b453d4bbf7f5bb9faec30a2b78bdf7ca77a53
niwanaga <iwanaga@orange-cube.jp>
2015-07-08 14:44:20 +0900

    Add MinimumThreadingBridge

---
+++ src/MinimumThreadingBridge/Threading/CancellationToken.cs
@@ -0,0 +1,44 @@
+﻿namespace System.Threading
+{
+    public struct CancellationToken
+    {
+        private readonly CancellationTokenSource _source;
+
+        internal CancellationToken(CancellationTokenSource source) { _source = source; }
+
+        public bool IsCancellationRequested
+        {
+            get
+            {
+                if (_source == null)
+                    return false;
+                else
+                    return _source.IsCancellationRequested;
+            }
+        }
+
+        public void Register(Action callback)
+        {
+            if (_source != null)
+            {
+                if (_source.IsCancellationRequested) callback();
+                _source.Canceled += callback;
+            }
+        }
+
+        public static CancellationToken None { get; } = new CancellationToken();
+
+        public void ThrowIfCancellationRequested()
+        {
+            if (IsCancellationRequested)
+            {
+                throw new OperationCanceledException();
+            }
+        }
+
+        public static bool operator ==(CancellationToken x, CancellationToken y) => x._source == y._source;
+        public static bool operator !=(CancellationToken x, CancellationToken y) => x._source != y._source;
+        public override bool Equals(object obj) => obj is CancellationToken && _source == ((CancellationToken)obj)._source;
+        public override int GetHashCode() => _source.GetHashCode();
+    }
+}
---
+++ src/MinimumThreadingBridge/Threading/CancellationTokenSource.cs
@@ -0,0 +1,43 @@
+﻿namespace System.Threading
+{
+    public class CancellationTokenSource
+    {
+        public CancellationTokenSource()
+        {
+            Token = new CancellationToken(this);
+        }
+
+        public CancellationToken Token { get; }
+
+        public bool IsCancellationRequested { get; private set; }
+
+        public void Cancel()
+        {
+            if (IsCancellationRequested) return;
+
+            var d = _canceled;
+            if (d != null) d();
+            d = null;
+
+            IsCancellationRequested = true;
+        }
+
+        internal event Action Canceled
+        {
+            add
+            {
+                if (IsCancellationRequested)
+                {
+                    if(value != null)
+                        value();
+                }
+                else
+                {
+                    _canceled += value;
+                }
+            }
+            remove { _canceled -= value; }
+        }
+        private Action _canceled;
+    }
+}
