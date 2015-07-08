b5675ff783029c3733343bc8842ae0572381b404
niwanaga <iwanaga@orange-cube.jp>
2015-07-08 14:44:34 +0900

    update nuspec

--- src/MinimumAsyncBridge.Nuget/pack.bat
+++ src/MinimumAsyncBridge.Nuget/pack.bat
@@ -1,2 +1,3 @@
 .\NuGet.exe pack .\MinimumAsyncBridge.nuspec -NoPackageAnalysis -OutputDirectory .
+.\NuGet.exe pack .\MinimumThreadingBridge.nuspec -NoPackageAnalysis -OutputDirectory .
 .\NuGet.exe pack .\IteratorTasks.AsyncBridge.nuspec -NoPackageAnalysis -OutputDirectory .
