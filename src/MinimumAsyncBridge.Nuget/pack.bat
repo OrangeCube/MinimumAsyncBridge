50695bea2039bdcb0bfec9f2c812d37af0103bbf
niwanaga <iwanaga@orange-cube.jp>
2015-07-10 12:29:49 +0900

    MvvmBridge

--- src/MinimumAsyncBridge.Nuget/pack.bat
+++ src/MinimumAsyncBridge.Nuget/pack.bat
@@ -1,3 +1,4 @@
 .\NuGet.exe pack .\MinimumAsyncBridge.nuspec -NoPackageAnalysis -OutputDirectory .
 .\NuGet.exe pack .\MinimumThreadingBridge.nuspec -NoPackageAnalysis -OutputDirectory .
 .\NuGet.exe pack .\IteratorTasks.AsyncBridge.nuspec -NoPackageAnalysis -OutputDirectory .
+.\NuGet.exe pack .\MvvmBridge.nuspec -NoPackageAnalysis -OutputDirectory .
---
+++ src/MvvmBridge.TypeForward/TypeForward.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(System.Collections.Specialized.INotifyCollectionChanged))]
+[assembly: TypeForwardedTo(typeof(System.Collections.Specialized.NotifyCollectionChangedAction))]
+[assembly: TypeForwardedTo(typeof(System.Collections.Specialized.NotifyCollectionChangedEventArgs))]
+[assembly: TypeForwardedTo(typeof(System.Collections.Specialized.NotifyCollectionChangedEventHandler))]
+[assembly: TypeForwardedTo(typeof(System.Collections.ObjectModel.ObservableCollection<>))]
