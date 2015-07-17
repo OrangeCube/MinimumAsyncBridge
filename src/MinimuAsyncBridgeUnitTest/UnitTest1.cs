7dfc20b7b1e9cca757ac2df2dc370949b0c01941
ppc <ppc@orange-cube.jp>
2015-07-17 12:18:30 +0900

    Add TestCase
    - `Task.Result` should has exception if it get exception or cancels.
    - `Task.Result` should wait if it is running.
    - `Task.Delay` should ignore cancellation when `CancellationTokenSource.Cancel` is called twice or more.
    - Await `Task` should has exception if it get exception or cancels.

--- src/MinimuAsyncBridgeUnitTest/UnitTest1.cs
+++ src/MinimuAsyncBridgeUnitTest/UnitTest1.cs
@@ -132,7 +132,7 @@ namespace MinimuAsyncBridgeUnitTest
         }
 
         [TestMethod]
-        public void WhenAnyで最初に例外で終わった場合はResultに例外がでたタスクが返る()
+        public void WhenAnyShouldHaveExceptionIfFirstItemOfTheTasksGetError()
         {
             WhenAnyShouldHaveExceptionIfFirstItemOfTheTasksGetErrorAsync().Wait();
             WhenAnyShouldHaveExceptionIfFirstItemOfTheTasksOfTResultGetErrorAsync().Wait();
@@ -147,7 +147,18 @@ namespace MinimuAsyncBridgeUnitTest
             var r = await Task.WhenAny<int>(tcs.Task, t2);
 
             Assert.AreSame(r, tcs.Task);
-            Assert.AreEqual(r.Result, default(int));
+
+            var exceptionCount = 0;
+            try
+            {
+                var result = r.Result;
+            }
+            catch(Exception e)
+            {
+                Assert.AreEqual(e.GetType(), typeof(TaskCanceledException));
+                exceptionCount++;
+            }
+            Assert.AreEqual(exceptionCount, 1);
         }
 
         private async Task WhenAnyShouldBeCanceledIfFirstItemOfTheTasksIsCancledAsync()
@@ -164,32 +175,46 @@ namespace MinimuAsyncBridgeUnitTest
         private async Task WhenAnyShouldHaveExceptionIfFirstItemOfTheTasksOfTResultGetErrorAsync()
         {
             var tcs = new TaskCompletionSource<int>();
-            Task.Delay(10).ContinueWith(_ => tcs.SetException(new Exception("planned exception")));
+            var ex = new Exception("planned exception");
+            Task.Delay(10).ContinueWith(_ => tcs.SetException(ex));
             var t2 = Task.Delay(500).ContinueWith(_ => 2);
 
             var r = await Task.WhenAny<int>(tcs.Task, t2);
 
             Assert.AreSame(r, tcs.Task);
-            Assert.AreEqual(r.Result, default(int));
+
+            var exceptionCount = 0;
+            try
+            {
+                var result = r.Result;
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e, ex);
+                exceptionCount++;
+            }
+            Assert.AreEqual(exceptionCount, 1);
         }
 
         private async Task WhenAnyShouldHaveExceptionIfFirstItemOfTheTasksGetErrorAsync()
         {
             var tcs = new TaskCompletionSource<object>();
-            Task.Delay(10).ContinueWith(_ => tcs.SetException(new Exception("planned exception")));
+            var plannedException = new Exception("planned exception");
+            Task.Delay(10).ContinueWith(_ => tcs.SetException(plannedException));
             var t2 = Task.Delay(500);
 
             var r = await Task.WhenAny(tcs.Task, t2);
 
             Assert.AreSame(r, tcs.Task);
+            Assert.AreSame(r.Exception, plannedException);
         }
 
         private async Task WhenAnyDelaysShouldAwaitForMinDelayWithTResult()
         {
             var t1 = Task.Delay(10).ContinueWith(_ => 1);
-            var t2 = Task.Delay(20).ContinueWith(_ => 2);
-            var t3 = Task.Delay(50).ContinueWith(_ => 3);
-            var t4 = Task.Delay(100).ContinueWith(_ => 4);
+            var t2 = Task.Delay(200).ContinueWith(_ => 2);
+            var t3 = Task.Delay(500).ContinueWith(_ => 3);
+            var t4 = Task.Delay(1000).ContinueWith(_ => 4);
 
             var r = await Task.WhenAny<int>(
                 t2,
@@ -204,9 +229,9 @@ namespace MinimuAsyncBridgeUnitTest
         private async Task WhenAnyDelaysShouldAwaitForMinDelay()
         {
             var t1 = Task.Delay(10);
-            var t2 = Task.Delay(20);
-            var t3 = Task.Delay(50);
-            var t4 = Task.Delay(100);
+            var t2 = Task.Delay(200);
+            var t3 = Task.Delay(500);
+            var t4 = Task.Delay(1000);
 
             var r = await Task.WhenAny(
                 t2,
@@ -218,28 +243,290 @@ namespace MinimuAsyncBridgeUnitTest
         }
 
         [TestMethod]
-        public void TestTaskCompletionSource()
+        public void TestTaskCompletionSource_Result()
         {
-            TrySetResultShouldWorkIifFirstTime().Wait();
+            GetResultShouldHaveExceptionIfTheTasksGetError();
+            GetResultShouldHaveTaskCanceledExceptionIfTheTasksIsCanceled();
+            GetResultShouldWaitIfTheTasksIsRunning();
         }
 
-        private async Task TrySetResultShouldWorkIifFirstTime()
+        private void GetResultShouldHaveExceptionIfTheTasksGetError()
         {
             var tcs = new TaskCompletionSource<int>();
+            var ex = new Exception();
+            tcs.SetException(ex);
+            var exceptionCount = 0;
+
+            try
+            {
+                var res = tcs.Task.Result;
+            }
+            catch (Exception e)
+            {
+                // todo AggregateException
+                Assert.AreEqual(e, ex);
+                exceptionCount++;
+            }
+            Assert.AreEqual(exceptionCount, 1);
+        }
+
+        private void GetResultShouldHaveTaskCanceledExceptionIfTheTasksIsCanceled()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            tcs.SetCanceled();
+            var exceptionCount = 0;
+
+            try
+            {
+                var res = tcs.Task.Result;
+            }
+            catch (Exception e)
+            {
+                // todo AggregateException
+                Assert.AreEqual(e.GetType(), typeof(TaskCanceledException));
+                exceptionCount++;
+            }
+            Assert.AreEqual(exceptionCount, 1);
+        }
+
+        private void GetResultShouldWaitIfTheTasksIsRunning()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            Task.Delay(200).ContinueWith(_ => tcs.SetResult(321));
+            var res = tcs.Task.Result;
+            Assert.AreEqual(res, 321);
+        }
+
+        [TestMethod]
+        public void TestTaskCompletionSource_SetXxx()
+        {
+            SetResultShouldHaveExceptionIfSecondTimeCalls().Wait();
+            SetCanceledShouldHaveExceptionIfSecondTimeCalls().Wait();
+            SetExceptionShouldHaveExceptionIfSecondTimeCalls().Wait();
+        }
+
+        private async Task SetResultShouldHaveExceptionIfSecondTimeCalls()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            var exceptionCount = 0;
+            tcs.SetResult(1);
+            try
+            {
+                tcs.SetResult(2);
+            }
+            catch(Exception e)
+            {
+                Assert.AreEqual(e.GetType(), typeof(InvalidOperationException));
+                exceptionCount++;
+            }
+
+            try
+            {
+                tcs.SetCanceled();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.GetType(), typeof(InvalidOperationException));
+                exceptionCount++;
+            }
+
+            try
+            {
+                tcs.SetException(new Exception());
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.GetType(), typeof(InvalidOperationException));
+                exceptionCount++;
+            }
+            Assert.AreEqual(exceptionCount, 3);
+
+            var r = await tcs.Task;
+
+            Assert.AreEqual(r, 1);
+        }
+
+        private async Task SetCanceledShouldHaveExceptionIfSecondTimeCalls()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            var exceptionCount = 0;
+            tcs.SetCanceled();
+            try
+            {
+                tcs.SetResult(2);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.GetType(), typeof(InvalidOperationException));
+                exceptionCount++;
+            }
+
+            try
+            {
+                tcs.SetCanceled();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.GetType(), typeof(InvalidOperationException));
+                exceptionCount++;
+            }
+
+            try
+            {
+                tcs.SetException(new Exception());
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.GetType(), typeof(InvalidOperationException));
+                exceptionCount++;
+            }
+
+            try
+            {
+                var r = await tcs.Task;
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.GetType(), typeof(TaskCanceledException));
+                exceptionCount++;
+            }
+
+            Assert.IsNull(tcs.Task.Exception);
+            Assert.AreEqual(exceptionCount, 4);
+        }
+
+        private async Task SetExceptionShouldHaveExceptionIfSecondTimeCalls()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            var exceptionCount = 0;
+            tcs.SetException(new Exception("first"));
+            try
+            {
+                tcs.SetResult(2);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.GetType(), typeof(InvalidOperationException));
+                exceptionCount++;
+            }
+
+            try
+            {
+                tcs.SetCanceled();
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.GetType(), typeof(InvalidOperationException));
+                exceptionCount++;
+            }
+
+            try
+            {
+                tcs.SetException(new Exception("second"));
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.GetType(), typeof(InvalidOperationException));
+                exceptionCount++;
+            }
+
+            try
+            {
+                var r = await tcs.Task;
+            }
+            catch(Exception e)
+            {
+                Assert.AreEqual(e.GetType(), typeof(Exception));
+                Assert.AreEqual(e.Message, "first");
+                exceptionCount++;
+            }
+
+            Assert.IsNotNull(tcs.Task.Exception);
+            Assert.AreEqual(exceptionCount, 4);
+        }
+
+        [TestMethod]
+        public void TestTaskCompletionSource_TrySetXxx()
+        {
+            TrySetResultShouldWorkIfFirstTime().Wait();
+            TrySetCanceledShouldWorkIfFirstTime().Wait();
+            TrySetExceptionShouldWorkIfFirstTime().Wait();
+        }
+
+        private async Task TrySetResultShouldWorkIfFirstTime()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            var completedCount = 0;
+            tcs.Task.ContinueWith(t => completedCount++);
             tcs.TrySetResult(1);
             tcs.TrySetResult(2);
+            tcs.TrySetCanceled();
+            tcs.TrySetException(new Exception());
 
             var res = await tcs.Task.ConfigureAwait(true);
-            Assert.IsTrue(res == 1);
+            Assert.AreEqual(completedCount, 1);
+            Assert.AreEqual(res, 1);
+        }
+
+        private async Task TrySetCanceledShouldWorkIfFirstTime()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            var completedCount = 0;
+            tcs.Task.ContinueWith(t => completedCount++);
+            tcs.TrySetCanceled();
+            tcs.TrySetCanceled();
+            tcs.TrySetResult(1);
+            tcs.TrySetException(new Exception());
+
+            var exceptionCount = 0;
+            try
+            {
+                var res = await tcs.Task.ConfigureAwait(true);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.GetType(), typeof(TaskCanceledException));
+                exceptionCount++;
+            }
+
+            Assert.AreEqual(completedCount, 1);
+            Assert.AreEqual(exceptionCount, 1);
+            Assert.AreEqual(tcs.Task.Status, TaskStatus.Canceled);
+        }
+
+        private async Task TrySetExceptionShouldWorkIfFirstTime()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            var completedCount = 0;
+            tcs.Task.ContinueWith(t => completedCount++);
+            tcs.TrySetException(new Exception("first"));
+            tcs.TrySetException(new Exception("second"));
+            tcs.TrySetResult(1);
+            tcs.TrySetCanceled();
+
+            var exceptionCount = 0;
+            try
+            {
+                var res = await tcs.Task.ConfigureAwait(true);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.Message, "first");
+                exceptionCount++;
+            }
+
+            Assert.AreEqual(completedCount, 1);
+            Assert.AreEqual(exceptionCount, 1);
+            Assert.AreEqual(tcs.Task.Status, TaskStatus.Faulted);
         }
 
         [TestMethod]
         public void TestCancellationTokenSource()
         {
-            CancellationTokenSource_CancelShouldWorkIifFirstTime();
+            CancellationTokenSource_CancelShouldWorkIfFirstTime();
         }
 
-        private void CancellationTokenSource_CancelShouldWorkIifFirstTime()
+        private void CancellationTokenSource_CancelShouldWorkIfFirstTime()
         {
             var cancelCount = 0;
             var cts = new CancellationTokenSource();
@@ -247,7 +534,47 @@ namespace MinimuAsyncBridgeUnitTest
             cts.Cancel();
 
             Assert.IsTrue(cancelCount == 1);
+        }
 
+        [TestMethod]
+        public void TaskDelayShouldIgnoreCancelAfterIsCompleted()
+        {
+            TaskDelayShouldCancelIfCanceledAsync().Wait();
+        }
+
+        private async Task TaskDelayShouldCancelIfCanceledAsync()
+        {
+            var cts = new CancellationTokenSource();
+            var d = Task.Delay(10, cts.Token);
+
+            await d;
+
+            await Task.Delay(1);
+
+            cts.Cancel();
+        }
+
+        [TestMethod]
+        public void TaskDelayCanCancel()
+        {
+            TaskDelayCanCancelAsync().Wait();
+        }
+
+        private async Task TaskDelayCanCancelAsync()
+        {
+            var cts1 = new CancellationTokenSource();
+            var d1 = Task.Delay(1000, cts1.Token);
+            cts1.Cancel();
+            Assert.AreEqual(d1.Status, TaskStatus.Canceled);
+
+            var cts2 = new CancellationTokenSource();
+            var d2 = Task.Delay(1000, cts2.Token);
+
+            await Task.Delay(10);
+
+            cts2.Cancel();
+
+            Assert.AreEqual(d2.Status, TaskStatus.Canceled);
         }
     }
 }
