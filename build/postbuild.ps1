6a8fb2bbba5bbd6b4ef1b3a89839d86c8865ffaf
niwanaga <iwanaga@orange-cube.jp>
2015-08-17 11:42:17 +0900

    build

---
+++ build/postbuild.ps1
@@ -0,0 +1,6 @@
+﻿pushd ..\src\MinimumAsyncBridge.Nuget
+.\pack.bat
+popd
+
+if (-not (Test-Path ../../packages)) { mkdir ../../packages }
+cp ..\src\MinimumAsyncBridge.Nuget\*.nupkg ../../packages
--- src/MinimumAsyncBridge.Nuget/pack.bat
+++ src/MinimumAsyncBridge.Nuget/pack.bat
@@ -1,4 +1,3 @@
 .\NuGet.exe pack .\MinimumAsyncBridge.nuspec -NoPackageAnalysis -OutputDirectory .
 .\NuGet.exe pack .\MinimumThreadingBridge.nuspec -NoPackageAnalysis -OutputDirectory .
-.\NuGet.exe pack .\IteratorTasks.AsyncBridge.nuspec -NoPackageAnalysis -OutputDirectory .
 .\NuGet.exe pack .\MvvmBridge.nuspec -NoPackageAnalysis -OutputDirectory .
