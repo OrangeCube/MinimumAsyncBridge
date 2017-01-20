09290c2eebb0f45715774e3e8f4fd9210b0c69f3
syasui <syasui@orange-cube.jp>
2017-01-20 21:10:34 +0900

    call SetSynchronizationContext before and after OnCompleted

--- src/MinimumAsyncBridge/Runtime/CompilerServices/ConfiguredTaskAwaitable`1.cs
+++ src/MinimumAsyncBridge/Runtime/CompilerServices/ConfiguredTaskAwaitable`1.cs
@@ -27,8 +27,20 @@ namespace System.Runtime.CompilerServices
                 _capturedContext = continueOnCapturedContext ? SynchronizationContext.Current : null;
             }
 
-            public void OnCompleted(Action continuation) => TaskAwaiter.OnCompletedInternal(_t, continuation, _capturedContext);
-            public void UnsafeOnCompleted(Action continuation) => OnCompleted(continuation);
+            public void OnCompleted(Action continuation)
+            {
+                var back = SynchronizationContext.Current;
+                SynchronizationContext.SetSynchronizationContext(_capturedContext);
+                TaskAwaiter.OnCompletedInternal(_t, continuation, _capturedContext);
+                SynchronizationContext.SetSynchronizationContext(back);
+            }
+            public void UnsafeOnCompleted(Action continuation)
+            {
+                var back = SynchronizationContext.Current;
+                SynchronizationContext.SetSynchronizationContext(_capturedContext);
+                OnCompleted(continuation);
+                SynchronizationContext.SetSynchronizationContext(back);
+            }
             public bool IsCompleted => _t.IsCompleted;
             public TResult GetResult() => _t.GetResult();
         }
