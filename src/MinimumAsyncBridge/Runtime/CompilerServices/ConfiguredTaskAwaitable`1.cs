493724e431d17a0619cb001196e815a6ad7c7be2
syasui <syasui@orange-cube.jp>
2017-01-23 14:41:38 +0900

    must resotre SynchronizationContext

--- src/MinimumAsyncBridge/Runtime/CompilerServices/ConfiguredTaskAwaitable`1.cs
+++ src/MinimumAsyncBridge/Runtime/CompilerServices/ConfiguredTaskAwaitable`1.cs
@@ -28,16 +28,28 @@ namespace System.Runtime.CompilerServices
             public void OnCompleted(Action continuation)
             {
                 var back = SynchronizationContext.Current;
-                SynchronizationContext.SetSynchronizationContext(_capturedContext);
-                TaskAwaiter.OnCompletedInternal(_t, continuation, _capturedContext);
-                SynchronizationContext.SetSynchronizationContext(back);
+                try
+                {
+                    SynchronizationContext.SetSynchronizationContext(_capturedContext);
+                    TaskAwaiter.OnCompletedInternal(_t, continuation, _capturedContext);
+                }
+                finally
+                {
+                    SynchronizationContext.SetSynchronizationContext(back);
+                }
             }
             public void UnsafeOnCompleted(Action continuation)
             {
                 var back = SynchronizationContext.Current;
-                SynchronizationContext.SetSynchronizationContext(_capturedContext);
-                OnCompleted(continuation);
-                SynchronizationContext.SetSynchronizationContext(back);
+                try
+                {
+                    SynchronizationContext.SetSynchronizationContext(_capturedContext);
+                    OnCompleted(continuation);
+                }
+                finally
+                {
+                    SynchronizationContext.SetSynchronizationContext(back);
+                }
             }
             public bool IsCompleted => _t.IsCompleted;
             public TResult GetResult() => _t.GetResult();
