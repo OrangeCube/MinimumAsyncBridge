0dd3f44b14a8fedba884556f6faf5f053f82252c
Nobuyuki Iwanaga <ufcpp@live.jp>
2018-05-31 16:40:49 +0900

    update ST.Tasks.Extensions PackageReference to 4.5
    support non-generic ValueTask.

--- src/TasksExtensions/TasksExtensionsBridge.TypeForward/TypeForward.cs
+++ src/TasksExtensions/TasksExtensionsBridge.TypeForward/TypeForward.cs
@@ -1,8 +1,13 @@
 ﻿using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
+[assembly: TypeForwardedTo(typeof(ValueTask))]
 [assembly: TypeForwardedTo(typeof(ValueTask<>))]
+[assembly: TypeForwardedTo(typeof(ValueTaskAwaiter))]
 [assembly: TypeForwardedTo(typeof(ValueTaskAwaiter<>))]
+[assembly: TypeForwardedTo(typeof(ConfiguredValueTaskAwaitable))]
 [assembly: TypeForwardedTo(typeof(ConfiguredValueTaskAwaitable<>))]
+[assembly: TypeForwardedTo(typeof(AsyncValueTaskMethodBuilder))]
 [assembly: TypeForwardedTo(typeof(AsyncValueTaskMethodBuilder<>))]
 [assembly: TypeForwardedTo(typeof(AsyncMethodBuilderAttribute))]
+[assembly: TypeForwardedTo(typeof(System.Threading.Tasks.Sources.IValueTaskSource))]
